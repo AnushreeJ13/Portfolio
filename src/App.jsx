64017447842d32863a539dc722cfebed8c08eeb5
AnushreeJ13 <Jainanushree2005@gmail.com>
2025-02-01 02:03:15 +0530

    app,jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,13 @@ import Technologies from './components/Technologies'
 import Experience from './components/Experience'
 import Contact from './components/Contact'
 import Education from './components/Education'
+
 const App = () => {
   return (
     <div className='overflow-x-hidden text-neutral-300 antialiased selection:bg-cyan-300 selection:text-cyan-900'>
       <div className="fixed top-0 -z-10 h-full w-full">
-<<<<<<< HEAD
+        {/* Choose the block you want */}
         <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
-=======
-      <div class="relative h-full w-full bg-slate-950"><div class="absolute bottom-0 left-[-20%] right-0 top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div><div class="absolute bottom-0 right-[-20%] top-[-10%] h-[500px] w-[500px] rounded-full bg-[radial-gradient(circle_farthest-side,rgba(255,0,182,.15),rgba(255,255,255,0))]"></div></div>
->>>>>>> e790b2a6524abf9e1088e3a942052c4a9034dbee
       </div>
       
       <div className="container mx-auto px-8">
